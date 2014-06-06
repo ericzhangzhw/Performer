89cb4cd6192d29cd4c0d41f026c67c603ad21d8c
Felix Chiu <felixchiu@Felixs-MacBook-Pro.local>
2014-06-06 10:41:31 -0700

    Initial

---
+++ src/main/java/com/nimblebook/framework/engine/ApplicationStarter.java
@@ -0,0 +1,11 @@
+package com.nimblebook.framework.engine;
+
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+
+		System.out.println("Something here happen big!");
+		
+	}
+
+}
